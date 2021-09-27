40e6ed2652dc9a25792554c783c8448ccce3336b
Jake <jaygarza1982@gmail.com>
2021-09-27 15:45:38 -0400

    Modified objects are saved server side

--- server/app.js
+++ server/app.js
@@ -5,36 +5,36 @@ const port = process.env.PORT || 3000;
 
 const getRandom = (max) => { return Math.random() * max; }
 
-let userDrawings = [
-  {
-    id: getRandom(500),
+let userDrawings = {
+  '1': {
+    id: '1',
     left: getRandom(500),
     top: getRandom(500),
     width: getRandom(100),
     height: getRandom(100)
   },
-  {
-    id: getRandom(500),
+  '2': {
+    id: '2',
     left: getRandom(500),
     top: getRandom(500),
     width: getRandom(100),
     height: getRandom(100)
   },
-  {
-    id: getRandom(500),
+  '3': {
+    id: '3',
     left: getRandom(500),
     top: getRandom(500),
     width: getRandom(100),
     height: getRandom(100)
   },
-  {
-    id: getRandom(500),
+  '4': {
+    id: '4',
     left: getRandom(500),
     top: getRandom(500),
     width: getRandom(100),
     height: getRandom(100)
-  }
-];
+  },
+};
 
 // Send drawings from all users
 // Users can use this when first joining
@@ -45,12 +45,19 @@ app.get('/api/objects', (req, res) => {
 io.on('connection', (socket) => {
 
   socket.on('object-event', msg => {
-    console.log('object event message', msg?.id);
+    try {
+      console.log(msg.id, 'was updated');
+  
+      // Set the object to the msg object
+      userDrawings[msg.id] = msg.object;
 
-    // TODO: Update userDrawings when this event fires
-    // TODO: This is so that new users will see the new changes
-    
-    io.emit('object-event', msg);
+      // Set the id of updated object, this is not in the msg.object, so it must be done here
+      userDrawings[msg.id].id = msg.id;
+      
+      io.emit('object-event', msg);
+    } catch (error) {
+      console.log('Unable to preform object-event', error);
+    }
   });
 
 });
